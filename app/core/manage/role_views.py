99bb30d55b2b80b219ef6b6c817d2b96550c83e5
DreamWFJ <wfj_sc@163.com>
2017-04-09 23:03:47 +0800

    add demo, such as user, role, email etc

--- app/core/manage/role_views.py
+++ app/core/manage/role_views.py
@@ -12,8 +12,8 @@ from app.core.db.sql.models import Role
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, login_required, logout_user, current_user
 from . import manage_blueprint as manage
-
-@manage.route('/roles')
-def role():
-    roles = Role.query.all()
-    return render_template('manage/role.html', roles=roles)
+#
+# @manage.route('/roles')
+# def role():
+#     roles = Role.query.all()
+#     return render_template('manage/role.html', roles=roles)
